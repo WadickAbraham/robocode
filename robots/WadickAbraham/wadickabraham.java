dbd0c3fb152cf000824b16e89941e5e3562faf9c
Wadick Abraham <xcraftpigz@gmail.com>
2024-06-06 20:34:29 -0400

    bit of work

---
+++ robots/WadickAbraham/wadickabraham.java
@@ -0,0 +1,39 @@
+package WadickAbraham;
+import robocode.Robot;
+import robocode.ScannedRobotEvent;
+import java.util.*;
+
+public class wadickabraham extends Robot{
+    public void run(){
+
+        //first write code that runs once
+        //eg. set the color
+        //get to a known position 
+
+
+
+        while(true){
+            //this repeasts forever until you die 
+            ahead(Math.random() * 400);
+            turnGunRight(90);
+
+            
+        
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+    }
+}
